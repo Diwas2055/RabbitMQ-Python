76b4b96002fa7323f5dfcee5fcda9465dc933c57
Diwash Bhandari <diwasb54@gmail.com>
2022-06-02 15:23:28 +0545

    Doc Added

--- 3- Publish-Subscribe/receive_logs.py
+++ 3- Publish-Subscribe/receive_logs.py
@@ -8,11 +8,15 @@ channel = connection.channel()
 # It's creating a new exchange if it doesn't already exist.
 channel.exchange_declare(exchange='logs', exchange_type='fanout')
 
-# It's creating a new queue, and the empty string as the queue name means that we're letting the
-# server choose a random queue name for us.
+# It's creating a new queue, and the empty string as the queue name means that we're letting 
+# the server choose a random queue name for us.
 result = channel.queue_declare(queue='', exclusive=True)
-queue_name = result.method.queue
 
+# It's creating a new queue, and the empty string as the queue name means that we're
+# letting the server choose a random queue name for us.
+queue_name =result.method.queue
+
+# It's binding the queue to the exchange.
 channel.queue_bind(exchange='logs', queue=queue_name)
 
 print(' [*] Waiting for logs. To exit press CTRL+C')
@@ -20,6 +24,7 @@ print(' [*] Waiting for logs. To exit press CTRL+C')
 def callback(ch, method, properties, body):
     print(" [x] %r" % body)
 
+# auto_ack=True ( It's telling RabbitMQ not to expect a reply from the consumer. ) 
 channel.basic_consume(
     queue=queue_name, on_message_callback=callback, auto_ack=True)
 
